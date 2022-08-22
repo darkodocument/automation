e211c301dfe44fe8459dec3679a581ac8f7d3d0f
irfan <darkodocument@gmail.com>
2022-08-22 17:24:31 +0530

    first commit

---
+++ automation/src/main/java/baseclass/AutoTest.java
@@ -0,0 +1,19 @@
+package baseclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.makemytrip.com/");
+		driver.close();
+	}
+
+}
